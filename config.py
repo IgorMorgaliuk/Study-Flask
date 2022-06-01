fa833884b0bcff8d034f39f3724b58e3ee03245b
IgorMorgaliuk <igormorgaliuk@gmail.com>
2022-06-01 16:27:04 +0300

    Pagination

--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['zang9873@gmail.com']
+    POSTS_PER_PAGE = 25
