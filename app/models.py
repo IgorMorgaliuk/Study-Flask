00f3ffb45274bac10bc77a3923d22c4d6b9b03ac
IgorMorgaliuk <igormorgaliuk@gmail.com>
2022-06-03 19:54:41 +0300

    add reset password гыÑusing flask-Mail

--- app/models.py
+++ app/models.py
@@ -1,9 +1,11 @@
-from app import db
+from app import db, app, login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from app import login
 from hashlib import md5
+from time import time
+import jwt
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,6 +23,11 @@ class User(UserMixin, db.Model):
         primaryjoin=(followers.c.follower_id == id),
         secondaryjoin=(followers.c.followed_id == id),
         backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256')
 
     def follow(self, user):
         if not self.is_following(user):
@@ -73,3 +80,12 @@ class Post(db.Model):
     @login.user_loader
     def load_user(id):
         return User.query.get(int(id))
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
